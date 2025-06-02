11c59b07e1bb09de23267ebd48a7f0963d27361a
victorMwatu <victormwatu@gmail.com>
2025-06-02 18:01:39 +0300

    complete js events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function (event){
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }else if(event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
